4f26b36b9caf9c7fd773f9850abd0b9bb623b602
Oleksiy Afanasyev Afanasyev <o.afanasyev@duocuc.cl>
2023-02-24 22:30:55 -0300

    first commit

---
+++ procesar.php
@@ -0,0 +1,10 @@
+<?php
+$nombre = $_GET['nombrecompleto'];
+$alias = $_GET['alias'];
+$rut = $_GET['rut'];
+$email = $_GET['email'];
+$region = $_GET['region'];
+$comuna = $_GET['comuna'];
+$candidato = $_GET['candidato'];
+$primercontacto = $_GET['opcion1'+'opcion2'+'opcion3'+'opcion4'];
+?>
